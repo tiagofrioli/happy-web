b2b310499ab23358a715ec665ec70c731adcb630
Tiago Frioli Leonel <tiago.frioli@gmail.com>
2020-10-29 22:08:52 -0300

    first commit

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -3,8 +3,16 @@ import { FiPlus } from 'react-icons/fi';
 import marker from '../images/marker.svg';
 import { Link } from 'react-router-dom';
 import '../styles/pages/orphanages-map.css';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: marker,
+    iconSize: [58,68],
+    iconAnchor:[29,68],
+    popupAnchor: [170,2], 
+});
 
 function OrphanagesMap(){
     return (
@@ -29,6 +37,12 @@ function OrphanagesMap(){
             >
                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>     */}
                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+                
+                <Marker icon={mapIcon} position={[-22.1502451,-51.3994681]}>
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar dos Meninos
+                    </Popup>
+                </Marker>
             </Map>    
 
             <Link to="" className="create-orphanage">
