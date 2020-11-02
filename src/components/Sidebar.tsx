8fc3dfe8b7467310184af7db37dd1044f1f04fd2
Tiago Frioli Leonel <tiago.frioli@gmail.com>
2020-11-02 16:30:57 -0300

    finish happy web

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { useHistory } from 'react-router-dom';
+import marker from '../images/marker.svg';
+import '../styles/components/Sidebar.css';
+
+const Sidebar: React.FC = () => {
+    const { goBack } = useHistory();
+
+  return (
+    <aside className="app-sidebar">
+        <img src={marker} alt="Happy"/>
+
+        <footer>
+            <button type="button" onClick={goBack}>
+                <FiArrowLeft size={24} color="#FFF" /> 
+            </button>
+        </footer>
+    </aside>
+
+  );
+}
+
+export default Sidebar;
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import React, { useEffect, useState } from 'react';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import marker from '../images/marker.svg';
 import { Link } from 'react-router-dom';
 import '../styles/pages/orphanages-map.css';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
 
-const mapIcon = Leaflet.icon({
-    iconUrl: marker,
-    iconSize: [58,68],
-    iconAnchor:[29,68],
-    popupAnchor: [170,2], 
-});
+interface Orphanage {
+    id: number;
+    latitude: number;
+    longitude: number;
+    name: string;
+}
 
 function OrphanagesMap(){
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+
+    useEffect(() =>{
+        api.get('orphanages').then(response => {
+          setOrphanages(response.data);
+
+        })
+    }, []);
+
     return (
         <div id="page-map">
             <aside>
@@ -38,14 +48,19 @@ function OrphanagesMap(){
                {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>     */}
                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
                 
-                <Marker icon={mapIcon} position={[-22.1502451,-51.3994681]}>
+               {orphanages.map(orphanage => {
+                   return (
+                    <Marker key={orphanage.id} icon={mapIcon} position={[orphanage.latitude,orphanage.longitude]}>
                     <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
-                        Lar dos Meninos
+                        {orphanage.name}
+                        <Link to={`/orphanages/${orphanage.id}`}><FiArrowRight size={20} color="#FFF"/></Link>
                     </Popup>
                 </Marker>
+                   )
+               })}
             </Map>    
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff" />
             </Link>
         </div>
